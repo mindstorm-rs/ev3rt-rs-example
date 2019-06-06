bf5aec6470635b28ed72c94fe99041297fa102db
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2019-06-06 18:46:59 +0200

    WIP on HRT.

--- src/lib.rs
+++ src/lib.rs
@@ -19,19 +19,23 @@ fn init(ev3: &mut Ev3) {
     ev3.mb().configure(MotorType::NONE);
     ev3.mc().configure(MotorType::NONE);
     ev3.md().configure(MotorType::NONE);
+
+    //ev3.apply_configuration();
 }
 
 #[no_mangle]
 pub extern "C" fn main_task() -> () {
     let mut ev3 = Ev3::new();
     init(&mut ev3);
-    ev3.apply_configuration();
 
-    ev3.screen.set_info_count(3);
+    /*
+    ev3.screen.set_info_count(1);
 
-    ev3.screen.setup_info(0, 4, 1, 3, 1, 1);
+    ev3.screen.setup_info(0, 4, 1, 1, 1, 1);
     ev3.screen
-        .setup_info_glyphs(0, &[Gph::T, Gph::E, Gph::S, Gph::T]);
+        .setup_info_glyphs(0, &[Gph::Minus, Gph::Minus, Gph::Minus, Gph::Minus]);
+    //ev3.screen
+    //    .setup_info_glyphs(0, &[Gph::T, Gph::E, Gph::S, Gph::T]);
 
     ev3.screen.setup_info(1, 8, 2, 3, 1, 1);
     ev3.screen
@@ -42,11 +46,26 @@ pub extern "C" fn main_task() -> () {
     ev3.screen
         .setup_info_glyphs(2, &[Gph::P, Gph::W, Gph::R, Gph::Space]);
     ev3.screen.setup_info_value(2, 4, 4);
+    */
 
-    let mut pwr = 0;
+    lcd_draw_string("LOOP", 2, 2);
+    lcd_draw_line(2, 0, 50, 0);
+
+    //let mut pwr = 0;
+    let mut x = 0;
     loop {
+        lcd_draw_string("LEAP", x + 2, x + 32);
+        lcd_draw_line(x + 2, x + 30, x + 50, x + 30);
+
+        x += 1;
+        if x > 100 {
+            x = 0;
+        }
+        usleep(100);
+
         ev3.read();
 
+        /*
         if ev3.keys.back.release_event() {
             abort();
         }
@@ -70,11 +89,20 @@ pub extern "C" fn main_task() -> () {
         } else {
             ev3.leds.reset();
         }
+        */
 
-        // let color = ev3.s1().val();
-        let color = 0;
-        ev3.screen.set_info_value(1, color);
-        ev3.screen.set_info_value(2, pwr);
+        if button_is_pressed(Button::RIGHT) {
+            led_set_color(LedColor::GREEN);
+        } else if button_is_pressed(Button::LEFT) {
+            led_set_color(LedColor::RED);
+        } else {
+            led_set_color(LedColor::OFF);
+        }
+
+        //let color = ev3.s1().val();
+        //let color = 0;
+        //ev3.screen.set_info_value(1, color);
+        //ev3.screen.set_info_value(2, pwr);
 
         ev3.apply();
     }
